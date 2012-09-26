f2cca2bfeba670414a588dfe66fea3f77342ac4a
David Silver <dsilver@candidatemetrics.com>
2012-09-26 15:11:01 -0700

    setting up

--- store/db/schema.rb
+++ store/db/schema.rb
@@ -17,8 +17,8 @@ ActiveRecord::Schema.define(:version => 20111005175852) do
     t.string   "name"
     t.decimal  "price"
     t.datetime "released_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
 end
